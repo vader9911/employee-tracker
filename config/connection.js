f75acf3f76e129a1c4c9a0df864aaaf9d35534be
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-03-23 15:35:52 -0400

    add .env.Example for login .env  example

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'your_database_name',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
